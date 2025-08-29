17702c53c15def8906269617f1f69792f6943956
AntoniZalewski <antekzalewski122@gmail.com>
2025-08-29 21:47:00 +0200

    feat: Create BookViewSet using ModelViewSet

--- src/library/views.py
+++ src/library/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookViewSet(ModelViewSet):
+    """
+    Mój ViewSet do zarządzania książkami.
+    Dzięki ModelViewSet, DRF automatycznie zapewni mi pełen zestaw operacji
+    CRUD (Create, Read, Update, Delete) bez pisania dodatkowej logiki.
+    """
+    # queryset to zbiór obiektów, na których ten ViewSet będzie operował.
+    # W tym przypadku - wszystkie książki.
+    queryset = Book.objects.all()
+
+    # serializer_class wskazuje "tłumacza", którego ViewSet ma używać.
+    serializer_class = BookSerializer
